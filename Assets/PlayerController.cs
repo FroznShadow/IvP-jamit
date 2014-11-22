f99dc8c7dded5242636fb678b6b5253961eb4a5c
unknown <TTV14SToniL@TA11L151-23.opetus.kajak.fi>
2014-11-22 12:42:29 +0200

    Taas kaikki kasaan

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 2;
+    public bool objectivecomplete = false;
+    public bool wincondition = false;
+	// Use this for initialization
+	void Start () 
+    {
+        
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(Input.GetKey(KeyCode.W))
+            transform.localPosition+=(transform.forward * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.localPosition -= (transform.forward * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.localPosition += (transform.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.localPosition -= (transform.right * speed * Time.deltaTime);
+        
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("joku osui");
+
+        if (col.gameObject.tag == "Objective")
+        {
+            Debug.Log("Osuttiin objectiveen");
+            objectivePickup();
+            objectivecomplete = true;
+        }
+        else if (col.gameObject.tag == "Exit" && objectivecomplete == true)
+        {
+            Debug.Log("Osuttiin Exit Blockiin");
+            exitScene(col);
+
+        }
+        else if (col.gameObject.tag == "win" && wincondition == true)
+        {
+            Debug.Log("Yaywinrar");
+            winthegame(col);
+        }
+    }
+    
+    public GameObject Objective;
+    public GameObject Exit;
+    public GameObject Exit2;
+    public GameObject Win;
+    void objectivePickup()
+    {
+       Objective.gameObject.SendMessage ("Gotit",SendMessageOptions.DontRequireReceiver); 
+    }
+    void exitScene(Collision col)
+    {
+        if(col.gameObject.name == "Exitblock")
+        Exit.gameObject.SendMessage("exit", SendMessageOptions.DontRequireReceiver);
+        else if (col.gameObject.name == "Exitblock2")
+        Exit2.gameObject.SendMessage("exit", SendMessageOptions.DontRequireReceiver);
+    }
+    void winthegame(Collision col)
+    {
+        Win.gameObject.SendMessage("win",SendMessageOptions.DontRequireReceiver);
+    }
+    
+}
+
---
+++ Assets/exittoolohuone.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exittoolohuone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void exit()
+    {
+        Application.LoadLevel("Olohuone");
+    }
+}
